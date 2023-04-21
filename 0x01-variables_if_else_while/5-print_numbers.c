b2c2860dd8fdb2e3294e108b2e836d430d88a557
Mickey-34 <abbeyella54@gmail.com>
2023-04-21 10:49:14 -0700

    Five_star

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - My Numbers
+ *
+ * Return: Must be 0
+ */
+int main(void)
+{
+	int united;
+
+	for (united = 0; united < 10; united++)
+	{
+		printf("%d", united);
+	}
+
+	printf("\n");
+
+	return (0);
+}
