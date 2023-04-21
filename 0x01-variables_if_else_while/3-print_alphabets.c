d074a9a57795b286c319aa6b22e6937f0f567be7
Mickey-34 <abbeyella54@gmail.com>
2023-04-21 10:00:02 -0700

    Alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Greater Alpha
+ *
+ * Return: Must be 0
+ */
+int main(void)
+{
+	char 1;
+	char 2;
+
+	for (1 = 'a'; 1 <= 'z'; 1++)
+	{
+		putchar(1);
+	}
+
+	for (2 = 'A'; 2 <= 'Z'; 2++)
+	{
+		putchar(2);
+	}
+
+	putchar('\n');
+	return (0);
+}
