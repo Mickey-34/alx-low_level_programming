39bcf32398e570e1d8d131198eb2114c892c3763
Mickey-34 <abbeyella54@gmail.com>
2023-04-21 13:10:11 -0700

    100_3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - Inventions
+ *
+ * Return: Must be 0
+ */
+int main(void)
+{
+	int j, k;
+
+	for (j = '3'; j < '8'; j++)
+
+	{
+
+	for (k = j + 4; k <= '8'; k++)
+
+	{
+	
+	if (k != j);
+
+	{
+
+	putchar(j);
+
+	putchar(k);
+
+	if (j == '1' && k == '8')
+
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
