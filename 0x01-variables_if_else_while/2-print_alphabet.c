c90666cbc4065abf47d51d80519447a20a282e50
Mickey-34 <abbeyella54@gmail.com>
2023-04-21 09:10:51 -0700

    Print-Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Alphabet Game
+ *
+ * Return: Must be 0
+ */
+int main(void)
+{
+	char mimi;
+
+	for (mimi = 'a'; mimi <= 'z';
+	mimi++)
+		putchar(mimi);
+	putchar('\n');
+	return (0);
+}
