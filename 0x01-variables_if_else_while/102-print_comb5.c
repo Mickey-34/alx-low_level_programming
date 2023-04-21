3deee635f66952eca0fab47f87817b022315014c
Mickey-34 <abbeyella54@gmail.com>
2023-04-21 13:40:25 -0700

    Combo_5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Must be 0
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 100; a++)
+	{
+		for (b = 0; b < 100; b++)
+		{
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+			if (a == 98 && q == 99)
+			continue;
+			putchar(',');
+			putchar(' ');
+
+			putchar('\n');
+			return (0)
+}
