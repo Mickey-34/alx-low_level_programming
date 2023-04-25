819d6d0c014131187417a579e1bdcfb788ef1628
Mickey-34 <abbeyella54@gmail.com>
2023-04-25 09:03:35 -0700

    Signs of numbers

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - Signs of numbers
+ * @n: Number to be checked of its sign
+ *
+ * Return: Return 1 for + numbers, 0 for 0, -1 for - numbers
+ */
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+	_putchar(77);
+	return (-1);
+	}
+	else if (n > 0)
+	{
+	_putchar(80);
+	return (1);
+	}
+	else
+	{
+	_putchar(83);
+	return (0);
+	}
+}
