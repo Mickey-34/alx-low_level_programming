3b3af96ecfe1e47525ddf2124f40acb6e30405b2
Mickey-34 <abbeyella54@gmail.com>
2023-04-25 07:21:17 -0700

    Lowercase_alpha

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Lowercase alphabets
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char mimi;
+
+	for (mimi = 'a'; mimi <= 'z'; mimi++)
+	{
+	_putchar(mimi);
+	}
+	_putchar('\n');
+}
