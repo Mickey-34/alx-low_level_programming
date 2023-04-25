83aa6abcbd542bb290460a6545efce526cec683b
Mickey-34 <abbeyella54@gmail.com>
2023-04-25 10:41:41 -0700

    Addition of integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Add 2 integers
+ * @z1: Represnting first integer
+ * @z2: Represnting second integer
+ *
+ * Return: Integer
+ */
+int add(int z1, int z2)
+{
+	return (z1 + z2);
+}
