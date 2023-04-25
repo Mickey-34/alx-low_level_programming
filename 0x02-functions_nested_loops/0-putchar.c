88cb3d2e460ef3a0d587fbbbbcb4b3606f8c4eb6
Mickey-34 <abbeyella54@gmail.com>
2023-04-25 06:26:53 -0700

    Putchar great

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - putchar
+ *
+ * Return: Must be 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	return (0);
+}
