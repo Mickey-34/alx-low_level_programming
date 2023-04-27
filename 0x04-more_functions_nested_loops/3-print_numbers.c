6268402e2746eaa7685079cfce66564ff847df28
Mickey-34 <abbeyella54@gmail.com>
2023-04-27 08:40:31 -0700

    Print n

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print numbers from 0-9
+ *
+ * Return: Void
+ */
+void print_numbers(void)
+{
+	char m;
+
+	for (m = '0'; m <= '9'; m++)
+	{
+	_putchar(m);
+	}
+	_putchar('\n');
+}
