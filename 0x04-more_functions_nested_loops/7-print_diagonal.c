b463648c089f15e66933977457ef48f5c68feeec
Mickey-34 <abbeyella54@gmail.com>
2023-04-27 10:23:49 -0700

    Diagonals

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,19 +10,14 @@ void print_diagonal(int n)
 	int c, d;
 
 	if (n <= 0)
-	{
 	_putchar('\n');
-	}
-	else
-	{
 	for (c = 0; c < n; c++)
 	{
 	for (d = 0; d < n; d++)
 	{
-	_putchar(30);
+	_putchar(' ');
 	}
-	_putchar(60);
+	_putchar('\\');
 	_putchar('\n');
-	}
 	}
 }
