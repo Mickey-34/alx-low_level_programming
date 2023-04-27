2a2fadb60574e69dee06035c9970c17a4915560e
Mickey-34 <abbeyella54@gmail.com>
2023-04-27 15:24:19 -0700

    cem

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Testing for negative and positive
+ *
+ * Return: Must be 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	postitive_or_negative(i);
+	return (0);
+}
