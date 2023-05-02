842e32b54f9a2b765b1efa364be897c3ae8ea321
Mickey-34 <abbeyella54@gmail.com>
2023-05-02 08:46:29 -0700

    Reverse string penta

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverse of a string
+ * @s: String to be reversed
+ * Return: Must be 0
+ */
+void rev_string(char *s)
+{
+	int mimi, box, palm;
+	char zoo;
+
+	for (mimi = 0; s[mimi] != '\0';
+	mimi++);
+	box = 0;
+	palm = mimi / 2;
+
+	while (palm--)
+	{
+	zoo = s[mimi - box - 1];
+	s[mimi - box - 1] = s[box];
+	s[box] = zoo;
+	box++;
+	}
+}
