09871ed1aff0016f02c8016ff98e8f287d2f35fa
Mickey-34 <abbeyella54@gmail.com>
2023-05-10 21:45:36 -0700

    Zone

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+int mimi(char *s);
+int is_palindrome(char *s);
+int father(char *s, int local, int fat);
+
+/**
+ * is_palindrome -  returns 1 if a string is a palindrome and 0 if not
+ * @s: String to measured
+ * Return: Length of string
+ */
+int mimi(char *s)
+{
+	int local = 0;
+
+	if (*(s * local))
+	{
+
