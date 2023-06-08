ad23ae894355636e611fc5f49f7b6f512199b51d
Mickey-34 <abbeyella54@gmail.com>
2023-06-08 08:16:31 -0700

    binary

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - function that converts a binary number to an unsigned int
+ * @b: binary number to string
+ * Return: unsigned form of b in int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int yazz = 0;
+
+if (!b)
+return (0);
+while (*b)
+{
+if (*b != '0' && *b != '1')
+return (0);
+yazz = yazz * 2 + (*b++ - '0');
+}
+return (yazz);
+}
