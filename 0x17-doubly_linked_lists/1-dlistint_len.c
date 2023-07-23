c7f7a60f68aa96295eab1bc64e675dcfa1c8f5f2
Mickey-34 <abbeyella54@gmail.com>
2023-07-23 05:28:08 -0700

    Task1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - get the length of a double-linked list
+ * @h: pointer to the head of the list
+ *
+ * Mimi Task
+ * Return: list length
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t len = 0;
+
+	for ( ; h; h = h->next)
+		++len;
+
+	return (len);
+}
